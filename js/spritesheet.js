d646f48d73be95aef4375af9425df945050682fe
Dobes Vandermeer <dobesv@gmail.com>
2013-04-23 21:42:40 -0700

    Merged sprite sheet

--- js/spritesheet.js
+++ js/spritesheet.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Created with TexturePacker (http://www.texturepacker.com)  Update key: $TexturePacker:SmartUpdate:2a533bce75b3eb1069d5f8076f515a76$
-=======
-// Created with TexturePacker (http://www.texturepacker.com)  Update key: $TexturePacker:SmartUpdate:421fca77c9fd127fd3ca00cf30f569a8$
->>>>>>> New sound toggle button - three-state button
+// Created with TexturePacker (http://www.texturepacker.com)  Update key: $TexturePacker:SmartUpdate:f27a77d697406b1aa4211a76f0700daf$
 (function() {
   var TP = (typeof(window.TexturePacker) == 'undefined' ? window.TexturePacker = {} : window.TexturePacker);
   if(typeof TP.images == 'undefined') TexturePacker.images = [];
@@ -15,74 +11,30 @@
   TP.frames["but_info_down"] = {x:313, y:461, w:155, h:50};
   TP.frames["but_info_hover"] = {x:157, y:461, w:155, h:50};
   TP.frames["but_info_up"] = {x:1, y:461, w:155, h:50};
-<<<<<<< HEAD
-  TP.frames["but_music_down"] = {x:621, y:439, w:75, h:62};
-  TP.frames["but_music_hover"] = {x:545, y:439, w:75, h:62};
-  TP.frames["but_music_up"] = {x:469, y:439, w:75, h:62};
-  TP.frames["but_nextlevel_down"] = {x:2411, y:414, w:169, h:43};
-  TP.frames["but_nextlevel_hover"] = {x:2152, y:438, w:169, h:43};
-  TP.frames["but_nextlevel_up"] = {x:1982, y:448, w:169, h:43};
-  TP.frames["but_reset_down"] = {x:2538, y:458, w:126, h:40};
-  TP.frames["but_reset_hover"] = {x:2411, y:458, w:126, h:40};
-  TP.frames["but_reset_up"] = {x:2021, y:157, w:126, h:40};
-=======
   TP.frames["but_nextlevel_down"] = {x:1954, y:238, w:169, h:43};
   TP.frames["but_nextlevel_hover"] = {x:1784, y:238, w:169, h:43};
   TP.frames["but_nextlevel_up"] = {x:1786, y:456, w:169, h:43};
   TP.frames["but_reset_down"] = {x:2210, y:438, w:126, h:40};
   TP.frames["but_reset_hover"] = {x:2083, y:438, w:126, h:40};
   TP.frames["but_reset_up"] = {x:1956, y:438, w:126, h:40};
-  TP.frames["but_sound_all"] = {x:2642, y:412, w:106, h:98};
-  TP.frames["but_sound_fx"] = {x:2535, y:412, w:106, h:98};
-  TP.frames["but_sound_mute"] = {x:2428, y:412, w:106, h:98};
->>>>>>> New sound toggle button - three-state button
+  TP.frames["but_sound_all"] = {x:2642, y:413, w:106, h:98};
+  TP.frames["but_sound_fx"] = {x:2535, y:413, w:106, h:98};
+  TP.frames["but_sound_mute"] = {x:2428, y:413, w:106, h:98};
   TP.frames["but_start_down"] = {x:720, y:328, w:128, h:110};
   TP.frames["but_start_hover"] = {x:591, y:328, w:128, h:110};
   TP.frames["but_start_up"] = {x:462, y:328, w:128, h:110};
   TP.frames["credit_text"] = {x:1, y:1, w:332, h:459};
   TP.frames["door_bottom"] = {x:334, y:1, w:767, h:326};
   TP.frames["door_top"] = {x:1102, y:1, w:681, h:294};
-<<<<<<< HEAD
-  TP.frames["filter_blue"] = {x:2927, y:1, w:100, h:100};
-  TP.frames["filter_clear"] = {x:2644, y:156, w:100, h:100};
-  TP.frames["filter_green"] = {x:2543, y:156, w:100, h:100};
-  TP.frames["filter_mirror"] = {x:2322, y:414, w:88, h:88};
-  TP.frames["filter_red"] = {x:2442, y:156, w:100, h:100};
-  TP.frames["filter_solid"] = {x:2341, y:157, w:100, h:100};
-  TP.frames["laser_blue"] = {x:2669, y:1, w:257, h:154};
-  TP.frames["laser_green"] = {x:2453, y:257, w:257, h:154};
-  TP.frames["laser_red"] = {x:2411, y:1, w:257, h:154};
-  TP.frames["level_complete"] = {x:2001, y:238, w:169, h:43};
-  TP.frames["level_number_0"] = {x:1910, y:238, w:62, h:53};
-  TP.frames["level_number_1"] = {x:1973, y:238, w:27, h:53};
-  TP.frames["level_number_2"] = {x:1847, y:238, w:62, h:53};
-  TP.frames["level_number_3"] = {x:1784, y:238, w:62, h:53};
-  TP.frames["level_number_4"] = {x:1843, y:452, w:62, h:53};
-  TP.frames["level_number_5"] = {x:1780, y:452, w:62, h:53};
-  TP.frames["level_number_6"] = {x:1717, y:452, w:62, h:53};
-  TP.frames["level_number_7"] = {x:1654, y:456, w:62, h:53};
-  TP.frames["level_number_8"] = {x:1591, y:456, w:62, h:53};
-  TP.frames["level_number_9"] = {x:1528, y:456, w:62, h:53};
-  TP.frames["level_number_display"] = {x:334, y:328, w:127, h:111};
-  TP.frames["pivot"] = {x:1784, y:1, w:236, h:236};
-  TP.frames["sensor_blue"] = {x:2258, y:258, w:194, h:155};
-  TP.frames["sensor_blue_off"] = {x:2216, y:1, w:194, h:155};
-  TP.frames["sensor_green"] = {x:2021, y:1, w:194, h:155};
-  TP.frames["sensor_green_off"] = {x:2063, y:282, w:194, h:155};
-  TP.frames["sensor_red"] = {x:1868, y:292, w:194, h:155};
-  TP.frames["sensor_red_off"] = {x:1673, y:296, w:194, h:155};
-  TP.frames["tutorial_1"] = {x:1139, y:296, w:388, h:205};
-  TP.frames["you_win"] = {x:2171, y:157, w:169, h:100};
-=======
-  TP.frames["filter_blue"] = {x:2528, y:156, w:100, h:100};
-  TP.frames["filter_clear"] = {x:2427, y:156, w:100, h:100};
+  TP.frames["filter_blue"] = {x:2528, y:157, w:100, h:100};
+  TP.frames["filter_clear"] = {x:2427, y:157, w:100, h:100};
   TP.frames["filter_green"] = {x:2326, y:157, w:100, h:100};
   TP.frames["filter_mirror"] = {x:2337, y:414, w:88, h:88};
   TP.frames["filter_red"] = {x:2225, y:157, w:100, h:100};
   TP.frames["filter_solid"] = {x:2124, y:157, w:100, h:100};
-  TP.frames["laser_blue"] = {x:2749, y:1, w:257, h:154};
-  TP.frames["laser_green"] = {x:2428, y:257, w:257, h:154};
-  TP.frames["laser_red"] = {x:2411, y:1, w:257, h:154};
+  TP.frames["laser_blue"] = {x:2749, y:156, w:257, h:154};
+  TP.frames["laser_green"] = {x:2606, y:1, w:257, h:154};
+  TP.frames["laser_red"] = {x:2428, y:258, w:257, h:154};
   TP.frames["level_complete"] = {x:1616, y:456, w:169, h:43};
   TP.frames["level_number_0"] = {x:1553, y:456, w:62, h:53};
   TP.frames["level_number_1"] = {x:973, y:429, w:27, h:53};
@@ -96,7 +48,7 @@
   TP.frames["level_number_9"] = {x:469, y:439, w:62, h:53};
   TP.frames["level_number_display"] = {x:334, y:328, w:127, h:111};
   TP.frames["pivot"] = {x:1784, y:1, w:236, h:236};
-  TP.frames["sensor_blue"] = {x:2686, y:156, w:193, h:154};
+  TP.frames["sensor_blue"] = {x:2411, y:1, w:194, h:155};
   TP.frames["sensor_blue_off"] = {x:2233, y:258, w:194, h:155};
   TP.frames["sensor_green"] = {x:2216, y:1, w:194, h:155};
   TP.frames["sensor_green_off"] = {x:2021, y:1, w:194, h:155};
@@ -104,5 +56,4 @@
   TP.frames["sensor_red_off"] = {x:1843, y:282, w:194, h:155};
   TP.frames["tutorial_1"] = {x:1164, y:296, w:388, h:205};
   TP.frames["you_win"] = {x:849, y:328, w:169, h:100};
->>>>>>> New sound toggle button - three-state button
 })();
